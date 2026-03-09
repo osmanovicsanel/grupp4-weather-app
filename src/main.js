5e4929a99f54b21ec0ee0edebdcc8bd602b24a26
Sanel Osmanovic <osmanovicsanel@hotmail.com>
2026-03-09 22:00:45 +0100

    Fixat stjärnan med sökfunktionen

--- src/main.js
+++ src/main.js
@@ -10,6 +10,9 @@ import { displayCurrentDate } from "./ui.js";
 import { showError, clearError } from "./ui.js";
 import { getFavorites, saveFavorite, removeFavorite } from "/src/storage.js";
 
+let currentActiveCity = "";
+
+
 const DEFAULT_CITY = "Gothenburg";
 
 // Håller koll på om geolocation redan jobbar - Maryam
@@ -20,9 +23,13 @@ document
   .querySelector(".search-bar")
   .addEventListener("keydown", async (event) => {
     if (event.key === "Enter") {
-      await handleSearch();
+      const city = event.target.value;
+      if (city) {
+        await loadWeather(city);
+        event.target.value = "";
+      }
     }
-});
+  });
 
 // Visar aktuellt datum i headern - Alvina
 displayCurrentDate();
@@ -36,7 +43,11 @@ const cityInput = document.getElementById("city-input");
  * @author Sanel
 */
 document.getElementById("search-btn").addEventListener("click", async () => {
-    await handleSearch();
+  const city = document.getElementById("city-input").value;
+  if (city) {
+    await loadWeather(city);
+    document.getElementById("city-input").value = "";
+  }
 });
 
 /**
@@ -52,6 +63,7 @@ async function loadWeather(city) {
     // Extrahera all data vi behöver
     const currentWeather = weatherData.current;
     const location = weatherData.location;
+    currentActiveCity = location.name; // Sanel
     const forecastDays = weatherData.forecast.forecastday;
     const todayForecast = forecastDays[0];
 
@@ -60,7 +72,7 @@ async function loadWeather(city) {
     renderAirQuality(currentWeather.air_quality); // Ivana
     renderWeatherDetails(currentWeather, todayForecast); // Ivana
     renderWeeklyForecast(forecastDays); // Maryam
-    updateStarState(city); // Sanel
+    updateStarState(currentActiveCity); // Sanel
 
     // Uppdatera datum i headern
     const date = new Date(location.localtime);
@@ -106,6 +118,8 @@ function loadWeatherByLocation() {
 
                 const currentWeather = weatherData.current;
                 const location = weatherData.location;
+                currentActiveCity = location.name; // Sanel
+                updateStarState(currentActiveCity); // Sanel
                 const forecastDays = weatherData.forecast.forecastday;
                 const todayForecast = forecastDays[0];
 
@@ -144,23 +158,29 @@ function updateStarState(city) {
 
   const favorites = getFavorites();
   if (favorites.includes(city)) {
-    favStar.classList.replace('fa-regular', 'fa-solid');
+    favStar.className = "fa-solid fa-star";
   } else {
-    favStar.classList.replace('fa-solid', 'fa-regular');
+    favStar.className = "fa-regular fa-star";
   }
 }
 
-/** Klick, sparar/tarbort favoriter
+/**
+ * Klick, sparar/tarbort favoriter
  * @author Sanel
  */
-document.getElementById('fav-star')?.addEventListener('click', () => {
-  const city = document.getElementById('city-name').textContent;
-  const favorites = getFavorites();
+document.addEventListener('click', (event) => {
+  if (event.target && event.target.id === 'fav-star') {
 
-  if (favorites.includes(city)) {
-    removeFavorite(city);
-  } else {
-    saveFavorite(city);
-  }
-  updateStarState(city);
-});
+    if (!currentActiveCity) return;
+    
+    const favorites = getFavorites();
+
+    if (favorites.includes(currentActiveCity)) {
+      removeFavorite(currentActiveCity);
+    } else {
+      saveFavorite(currentActiveCity);
+    }
+
+    updateStarState(currentActiveCity);
+    }
+  });
